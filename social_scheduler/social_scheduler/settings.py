1e0400a8454490607beec90a5b8fb5ac7e18391c
Samyak Choudhary <162186916+SAMurai-16@users.noreply.github.com>
2025-05-31 14:34:15 +0530

    Update settings.py

--- social_scheduler/social_scheduler/settings.py
+++ social_scheduler/social_scheduler/settings.py
@@ -178,7 +178,7 @@ LOGOUT_REDIRECT_URL = '/schedule/'
 
 # CELERY SETUP
 
-CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_BROKER_URL = config("REDIS_URL", default="redis://localhost:6379/0")
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
